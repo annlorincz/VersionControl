46e8665cb73145781b00f2a8dcfa68f8c00b8103
Anna Lőrincz <anna.lorincz@bcecid.net>
2019-12-01 20:19:42 +0100

    3

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -16,8 +16,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName; 
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName; 
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -34,8 +33,8 @@ namespace UserMaintenance
         {
             var u = new Entities.User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+                
             };
             users.Add(u);
         }
